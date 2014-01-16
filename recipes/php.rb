16064fafb0960e33ddca57e098a08275bdb541cb
G. Arends <galo@jpresult.nl>
2014-01-16 14:07:06 +0100

    add php directives

--- recipes/php.rb
+++ recipes/php.rb
@@ -10,6 +10,8 @@
 node.default['php']['packages'] = ["php5", "php5-dev", "php5-cli", "php-pear", "php5-gd", "php5-mysql"]
 node.default['php']['conf_dir'] = "/etc/php5/apache2"
 node.default['php']['directives']['user_agent'] = "."
+node.default['php']['directives']['upload_max_filesize'] = "8M"
+node.default['php']['directives']['post_max_size'] = "8M"
 
 include_recipe "php"
 
