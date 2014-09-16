d81ff61d0d902416f3481392e65b3fba6f637fd4
G. Arends <galo@jpresult.nl>
2014-09-16 18:24:32 -0400

    Change attributes syntax

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default['lamp']['sitesroot']  = "/var/www/sites"
+default.lamp.sitesroot = "/var/www/sites"
 
-default['lamp']['appname'] = "lamp"
-default['lamp']['approot']  = "/var/www/sites/#{lamp['appname']}"
-default['lamp']['site_url'] = ""
-default['lamp']['site_aliases'] = [""]
+default.lamp.appname = "lamp"
+default.lamp.approot = "#{lamp.sitesroot}/#{lamp.appname}"
+default.lamp.site_url = "localhost"
+default.lamp.site_aliases = ["localhost"]
