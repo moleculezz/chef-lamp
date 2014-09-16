b926f94ef25c8977bf29f22d231ac56abc398b81
G. Arends <galo@jpresult.nl>
2014-09-16 18:28:45 -0400

    Remove app specific php recipe config

--- recipes/php.rb
+++ recipes/php.rb
@@ -15,35 +15,4 @@ node.default.php.directives.post_max_size = "8M"
 
 include_recipe "php"
 
-dr = php_pear_channel "pear.drush.org" do
-  action :discover
-end
-
-php_pear "drush" do
-  channel dr.channel_name
-  version "6.2.0.0"
-  action :install
-end
-
-php_pear "zendopcache" do
-  version "7.0.3"
-  preferred_state "beta"
-  zend_extensions ['opcache.so']
-=begin
-directives(
-    :memory_consumption => 128,
-    :interned_strings_buffer => 8,
-    :max_accelerated_files => 4000,
-    :revalidate_freq => 60,
-    :fast_shutdown => 1,
-    :enable_cli => 1,
-    :save_comments => 0
-  )
-=end
-
-  action :install
-end
-
-package "imagemagick"
-
 include_recipe "composer"
