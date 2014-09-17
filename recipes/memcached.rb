d58f031f05d5926d949c5b2c1e3ca016caa89842
G. Arends <galo@jpresult.nl>
2014-09-17 00:49:27 -0400

    Change attribute syntax

--- recipes/memcached.rb
+++ recipes/memcached.rb
@@ -7,11 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node.default['memcached']['listen'] = "127.0.0.1"
+node.default.memcached.listen = "127.0.0.1"
 
 include_recipe "memcached"
 
 php_pear "memcache" do
   version "2.2.7"
-  action :install  
+  action :install
 end
