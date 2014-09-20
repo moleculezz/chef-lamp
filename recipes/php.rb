99184d5bf9aafa9b3107753f16da118e200961ed
G. Arends <galo@jpresult.nl>
2014-09-20 09:22:07 -0400

    Remove user agent php directive and move it to the application cookbook

--- recipes/php.rb
+++ recipes/php.rb
@@ -9,7 +9,7 @@
 
 node.default.php.packages = ["php5", "php5-dev", "php5-cli", "php-pear", "php5-gd", "php5-mysql"]
 node.default.php.conf_dir = "/etc/php5/apache2"
-node.default.php.directives.user_agent = "ua"
+
 node.default.php.directives.upload_max_filesize = "8M"
 node.default.php.directives.post_max_size = "8M"
 
